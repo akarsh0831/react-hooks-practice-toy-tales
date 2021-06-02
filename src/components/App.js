1b27b552c6d0e2c44092a8cae3f331b67a7605a4
akarsh <akarshshukla19@yahoo.com>
2021-06-01 23:12:08 -0400

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,59 @@
-import React, { useState } from "react";
-
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([])
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/toys`)
+      .then(resp => resp.json())
+      .then(toyArray => {
+        setToys(toyArray)
+      })
+  }, [])
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
 
+  const updateToys = (newToy) => {
+    const updatedToys = [...toys, newToy]
+    setToys(updatedToys)
+  }
+
+  const displayToys = (id) => {
+    const updatedToys = toys.filter(toy => {
+      return toy.id !== id
+    })
+
+    setToys(updatedToys)
+  }
+
+  const addUpdatedToToys = (toy) => {
+    const newToyList = toys.map(toyObj => {
+      if (toy.id === toyObj.id) {
+        console.log("toy", toy)
+        return toy
+      } else {
+        console.log("toyObj", toyObj)
+        return toyObj
+      }
+    })
+
+    setToys(newToyList)
+  }
+
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm updateToys={updateToys} /> : null}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer toys={toys} displayToys={displayToys} addUpdatedToToys={addUpdatedToToys}/>
     </>
   );
 }
